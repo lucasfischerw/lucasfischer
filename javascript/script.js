77dba909b0f0a38b7b3609fb2e2b86af76d9e4ab
LucasFischer-max <66845046+LucasFischer-max@users.noreply.github.com>
2020-08-31 11:16:08 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,12 @@
 function Carregou() {
+	preLoad("imagens/fundo.jpg")
 	setTimeout(function() {
 		document.getElementById('carregando').style.display = "none";
 		document.getElementById('content').style.display = "inherit";
 	}, 200)
+}
+
+function preLoad(url) {
+	var img = new Image();
+	img.src = url;
 }
