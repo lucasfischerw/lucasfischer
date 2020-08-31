71fe15ad140ad8f334966c3244b054f2abe61baf
LucasFischer-max <66845046+LucasFischer-max@users.noreply.github.com>
2020-08-30 22:44:46 -0300

    Add files via upload

--- desafios/memoria/javascript/script.js
+++ desafios/memoria/javascript/script.js
@@ -8,10 +8,13 @@ var totalJogadas = 0;
 var estaJogando = false;
 var vezesGanhou = 0;
 var tentativas = 0;
+var possivelAbrirProxima = true;
 
 function Jogar() {
 	if (estaJogando == false) {
 		estaJogando = true;
+		preloadImage()
+		document.body.style.backgroundColor = "rgb(40, 137, 217)"
 		document.getElementById("pontos").innerHTML = "Pontos: "+vezesGanhou+""
 		document.getElementById("tentativas").innerHTML = "Tentativas: "+tentativas+""
 		document.getElementById('botao').style.cursor = "not-allowed"
@@ -39,12 +42,16 @@ function Jogar() {
 }
 
 function clicou(numeroBotao) {
-	document.getElementById("carta-"+numeroBotao+"").style.backgroundImage = "url(imagens/"+imagem[numeroBotao - 1]+".png)"
-	document.getElementById("carta-"+numeroBotao+"").innerHTML = ""
-	document.getElementById("carta-"+numeroBotao+"").style.backgroundSize = "22vh 22vh"
-	totalJogadas = totalJogadas + 1;
-	if (totalJogadas == 2) {
-		pontuacao()
+	if (possivelAbrirProxima == true) {
+		document.getElementById("carta-"+numeroBotao+"").classList.add("comImagem")
+		document.getElementById("carta-"+numeroBotao+"").style.opacity = "0"
+		document.getElementById("carta-"+numeroBotao+"").style.backgroundImage = "url(imagens/"+imagem[numeroBotao - 1]+".png)"
+		document.getElementById("carta-"+numeroBotao+"").innerHTML = ""
+		document.getElementById("carta-"+numeroBotao+"").style.backgroundSize = "22vh 22vh"
+		totalJogadas = totalJogadas + 1;
+		if (totalJogadas == 2) {
+			pontuacao()
+		}
 	}
 }
 
@@ -66,54 +73,77 @@ function pontuacao() {
 	escolhido1 = 0;
 	totalJogadas = 0
 	escolhido2 = 0;
+	possivelAbrirProxima = false;
 	while(controle < 12) {
 		if (document.getElementById("carta-"+parseInt(controle+1)+"").style.backgroundImage.length > 10) {
 			if (document.getElementById("carta-"+parseInt(controle+1)+"").style.backgroundImage !== 'none') {
 				if (escolhido1 == 0) {
 					escolhido1 = controle+1
-					console.log(escolhido1)
 				} else {
 					escolhido2 = controle+1	
-					console.log(escolhido2)
 				}
 			}
 		}
 		controle = controle + 1	
 	}
-	if (document.getElementById("carta-"+escolhido1+"").style.backgroundImage == document.getElementById("carta-"+escolhido2+"").style.backgroundImage) {
-		setTimeout(function() {
-			document.getElementById("carta-"+escolhido1+"").classList.add("desaparecer")
-			document.getElementById("carta-"+escolhido2+"").classList.add("desaparecer")
-			vezesGanhou = vezesGanhou + 1;
-			tentativas = tentativas + 1;
-			document.getElementById("tentativas").innerHTML = "Tentativas: "+tentativas+""
-			document.getElementById("pontos").innerHTML = "Pontos: "+vezesGanhou+""
+	if (escolhido2 != 0) {
+		if (document.getElementById("carta-"+escolhido1+"").style.backgroundImage == document.getElementById("carta-"+escolhido2+"").style.backgroundImage) {
+			setTimeout(function() {
+				document.getElementById("carta-"+escolhido1+"").classList.add("desaparecer")
+				document.getElementById("carta-"+escolhido2+"").classList.add("desaparecer")
+				document.getElementById("carta-"+escolhido1+"").removeAttribute("onclick", "clicou("+parseInt(escolhido1)+")")
+				document.getElementById("carta-"+escolhido2+"").removeAttribute("onclick", "clicou("+parseInt(escolhido2)+")")
+				vezesGanhou = vezesGanhou + 1;
+				tentativas = tentativas + 1;
+				document.getElementById("tentativas").innerHTML = "Tentativas: "+tentativas+""
+				document.getElementById("pontos").innerHTML = "Pontos: "+vezesGanhou+""
+				setTimeout(function() {
+					document.getElementById("carta-"+escolhido1+"").style.backgroundImage = 'none'
+					document.getElementById("carta-"+escolhido2+"").style.backgroundImage = 'none'
+					document.getElementById("carta-"+escolhido1+"").classList.remove("comImagem")
+					document.getElementById("carta-"+escolhido2+"").classList.remove("comImagem")
+					possivelAbrirProxima = true;
+					if (vezesGanhou == 6) {
+						setTimeout(function() {
+							document.getElementById('linha1').innerHTML = "Ganhou em "+tentativas+" Tentativas"
+							document.getElementById('linha2').innerHTML = ""
+							if (tentativas < 12) {
+								document.getElementById('linha1').style.color = "rgb(70, 180, 70)"
+								document.body.style.backgroundColor = "rgb(70, 180, 70)"
+							} else if (tentativas < 20) {
+								document.getElementById('linha1').style.color = "rgb(240, 180, 0)"
+								document.body.style.backgroundColor = "rgb(240, 180, 0)"
+							} else {
+								document.getElementById('linha1').style.color = "rgb(255, 70, 70)"
+								document.body.style.backgroundColor = "rgb(255, 70, 70)"
+							}
+							document.getElementById('botao').innerHTML = "Reiniciar"
+							document.getElementById('botao').style.cursor = "pointer"
+							estaJogando = false;
+							possivelAbrirProxima = true;
+							resetar()
+						}, 200)
+					}
+				}, 500)
+			}, 500)
+		} else {
 			setTimeout(function() {
 				document.getElementById("carta-"+escolhido1+"").style.backgroundImage = 'none'
 				document.getElementById("carta-"+escolhido2+"").style.backgroundImage = 'none'
-				if (vezesGanhou == 6) {
-					resetar()
-					setTimeout(function() {
-						console.log('GANHOU')
-						document.getElementById('linha1').innerHTML = "Ganhou! Parabéns!"
-						document.getElementById('linha2').innerHTML = ""
-						document.getElementById('linha1').style.color = "rgb(70, 180, 70)"
-						document.getElementById('botao').innerHTML = "Reiniciar"
-						document.getElementById('botao').style.cursor = "pointer"
-						estaJogando = false;
-					}, 200)
-				}
-			}, 500)
-		}, 500)
+				document.getElementById("carta-"+escolhido1+"").innerHTML = ""+escolhido1+""
+				document.getElementById("carta-"+escolhido2+"").innerHTML = ""+escolhido2+""
+				document.getElementById("carta-"+escolhido1+"").classList.remove("comImagem")
+				document.getElementById("carta-"+escolhido2+"").classList.remove("comImagem")
+				document.getElementById("carta-"+escolhido1+"").style.opacity = "1"
+				document.getElementById("carta-"+escolhido2+"").style.opacity = "1"
+				tentativas = tentativas + 1
+				document.getElementById("tentativas").innerHTML = "Tentativas: "+tentativas+""
+				possivelAbrirProxima = true;
+			}, 800)
+		}
 	} else {
-		setTimeout(function() {
-			document.getElementById("carta-"+escolhido1+"").style.backgroundImage = 'none'
-			document.getElementById("carta-"+escolhido2+"").style.backgroundImage = 'none'
-			document.getElementById("carta-"+escolhido1+"").innerHTML = ""+escolhido1+""
-			document.getElementById("carta-"+escolhido2+"").innerHTML = ""+escolhido2+""
-			tentativas = tentativas + 1
-			document.getElementById("tentativas").innerHTML = "Tentativas: "+tentativas+""
-		}, 800)
+		possivelAbrirProxima = true;
+		totalJogadas = 1;
 	}
 }
 
@@ -129,12 +159,11 @@ function resetar() {
 	tentativas = 0;
 }
 
-function preloadImage(url) {
+function preloadImage() {
 	var controle = 1;
 	while(controle <= 6) {
-	    var img = new Image();
+		var img = new Image();
 		img.src = "imagens/"+controle+".png";
-		console.log(controle)
 		controle = controle + 1
 	}
 }
