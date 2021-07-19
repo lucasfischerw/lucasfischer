a9846763aff23d5461bbb713ccacb6a107af16df
LucasFischer-max <lucasfischerw@gmail.com>
2021-07-19 17:15:59 -0300

    Mudança de pastas

---
+++ projetos/tdah/javascript/script.js
@@ -0,0 +1,11 @@
+function redirect1() {
+	location.href = "https://www.testesonline.com.br/fazer-teste-online/teste-de-tdah";
+}
+
+function redirect2() {
+	location.href = "http://bvsms.saude.gov.br/dicas-em-saude/89-transtorno-do-deficit-de-atencao-com-hiperatividade-tdah";
+}
+
+function redirect3() {
+	location.href = "https://tdah.org.br/sobre-tdah/o-que-e-tdah/";
+}
