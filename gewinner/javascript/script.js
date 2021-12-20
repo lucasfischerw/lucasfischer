8f2bf71e978d6783b7381cda31573e280c94265d
LucasFischer-max <lucasfischerw@gmail.com>
2021-12-20 19:00:18 -0300

    Update Natal

--- gewinner/javascript/script.js
+++ gewinner/javascript/script.js
@@ -1,252 +1,150 @@
-var ultimaCor;
-var aberto;
-var bloquear = false;
-var paraCima = true;
-var emBaixo = 1;
+var Aberto = false;
 
-function openNav() {
-	if (!aberto) {
+function OpenNav() {
+	if(!Aberto) {
 		document.getElementById("menu").style.top = "-70px"
 		document.getElementById("sidebar").style.width = "250px";
 		document.getElementById("secoes").style.marginLeft = "250px";
 		document.getElementById("secoes").style.opacity = "0.5";
-		document.getElementById("arrow").style.cursor = "initial";
-		document.getElementById("arrow-container").style.bottom = "-40px";
+		document.getElementById("secoes").style.filter = "blur(5px)";
 		setTimeout(function() {
 			document.getElementById("itens").style.opacity = "1"
 			document.getElementById("itens").style.marginLeft = "0"
 		}, 250)
-		aberto = true;
+		Aberto = true;
 	}
 }
 
-function closeNav() {
-	if (aberto) {
+function CloseNav() {
+	if(Aberto) {
 		document.getElementById("menu").style.top = "0px"
 		document.getElementById("sidebar").style.width = "0px";
 		document.getElementById("secoes").style.marginLeft = "0px";
 		document.getElementById("secoes").style.opacity = "1";
-		document.getElementById("arrow").style.cursor = "pointer";
-		document.getElementById("arrow-container").style.bottom = "30px";
+		document.getElementById("secoes").style.filter = "none";
 		document.getElementById("itens").style.opacity = "0"
 		document.getElementById("itens").style.marginLeft = "-100px"
-		aberto = false;
+		Aberto = false;
 	}
 }
 
 var lastScrollTop = window.pageYOffset;
 
 function isScrolling() {
-	if (!bloquear) {
-		var st = window.pageYOffset || document.documentElement.scrollTop;
-		if (aberto) {
-			closeNav()
-		}
-		if (window.scrollY < 10) { 
-			document.getElementById("menu").style.top = "0px";
-			document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
+	var st = window.pageYOffset || document.documentElement.scrollTop;
+	if (st > lastScrollTop+0.5) {
+		document.getElementById("menu").style.top = "-70px";
+	} else {
+		document.getElementById("menu").style.top = "0px";
+		if(window.scrollY < 10) {
 			document.getElementById("menu").style.backdropFilter = "none";
-			document.getElementById("arrow").style.cursor = "pointer";
-			document.getElementById("arrow-container").style.bottom = "30px";
-			emBaixo = 1;
-			ultimaCor = "rgba(0, 0, 0, 0)"
-		} else if (st > lastScrollTop) {
-			document.getElementById("menu").style.top = "-70px";
-			document.getElementById("menu").style.backgroundColor = ultimaCor;
-			document.getElementById("arrow").style.cursor = "initial";
-			document.getElementById("arrow-container").style.bottom = "-40px";
-			emBaixo = 0;
+			document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
 		} else {
-			document.getElementById("menu").style.top = "0px";
-			if (window.scrollY < 100) {
-				document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-				document.getElementById("menu").style.backdropFilter = "none";
-			} else {
-				document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-				document.getElementById("menu").style.backdropFilter = "blur(5px)";	
-			}
-			document.getElementById("arrow").style.cursor = "pointer";
-			document.getElementById("arrow-container").style.bottom = "30px";
-			emBaixo = 1;
-			ultimaCor = "rgba(0, 0, 0, 0.5)"
-		}
-		lastScrollTop = st <= 0 ? 0 : st;
-	}
-}
-
-var timer = null;
-function scrollStop() {
-	if (timer !== null) {
-		clearTimeout(timer);
-	}
-	timer = setTimeout(function() {
-		if (bloquear) {
-			bloquear = false;
-		}
-		if (window.scrollY < 10 && paraCima == false) {
-			document.getElementById("arrow").style.transform = "rotate(360deg)"
-			document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-			document.getElementById("menu").style.backdropFilter = "none";
-			document.getElementById("menu").style.top = "0px";
-			paraCima = true;
-		} else if (document.getElementById("contato").getBoundingClientRect().top < 200) {
-			document.getElementById("arrow").style.transform = "rotate(180deg)"
-			document.getElementById("arrow").style.cursor = "pointer";
-			document.getElementById("arrow-container").style.bottom = "30px";
-			paraCima = false;
-		} else if (window.scrollY < 10) {
-			document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-			document.getElementById("menu").style.backdropFilter = "none";
-			document.getElementById("menu").style.top = "0px";
-		}
-	}, 150);
-}
-
-function scroll(id) {
-	bloquear = true
-	document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-	document.getElementById("menu").style.backdropFilter = "blur(5px)";
-	document.getElementById(id).scrollIntoView({ block: 'start',  behavior: 'smooth' });
-}
-
-function Integrantes() {
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("integrantes")
-		}, 500)
-	} else {
-		scroll("integrantes")
-	}
-}
-
-function Robos() {
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("robos")
-		}, 500)
-	} else {
-		scroll("robos")
-	}
-}
-
-function Competicoes() {
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("competicoes")
-		}, 500)
-	} else {
-		scroll("competicoes")
-	}
-}
-
-function Contato() {
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("contato")
-		}, 500)
-	} else {
-		scroll("contato")
-	}
-}
-
-function seta() {
-	if (!bloquear) {
-		var ids = ["inicial", "integrantes", "robos", "robos2", "competicoes", "competicoes2", "contato"]
-		for (var i = 0; i < ids.length; i++) {
-			if (paraCima && document.getElementById(ids[i]).getBoundingClientRect().top > 200) {
-				scroll(ids[i])
-				break;
-			} else if (!paraCima && document.getElementById(ids[i]).getBoundingClientRect().top > -200) {
-				scroll(ids[i-1])
-				break;
-			}
+			document.getElementById("menu").style.backdropFilter = "blur(5px)";
+			document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0.3)";
 		}
 	}
+	lastScrollTop = st <= 0 ? 0 : st;
 }
 
-function carregarAviso() {
-	if (localStorage.getItem("autoScroll") != 1) {
-		document.getElementById("aviso").style.display = "inherit";
+function Scroll(id) {
+	if(Aberto) {
+		CloseNav();
+		setTimeout(function() {
+			document.getElementById(id).scrollIntoView({ block: 'start',  behavior: 'smooth' });
+		}, 500)
 	} else {
-		carregar();
+		document.getElementById(id).scrollIntoView({ block: 'start',  behavior: 'smooth' });
 	}
 }
 
-function carregar() {
-	setTimeout(function() {
-		document.body.style.transition = "opacity 0.3s"
-		document.body.style.opacity = "0"
-		setTimeout(function() {
-			document.getElementById("carregando").style.display = "none"
-			document.getElementById("conteudo").style.display = "initial"
-			document.body.style.transition = "opacity 1s"
-			document.body.style.opacity = "1"
-			window.addEventListener('scroll', scrollStop, false);
-			if (localStorage.getItem("autoScroll") == 1) {
-				window.scrollTo(0, localStorage.getItem("scrollPosition"))
-				if (localStorage.getItem("scrollPosition") < 10) {
-					document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-					document.getElementById("menu").style.backdropFilter = "none";
-				} else {
-					document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-					document.getElementById("menu").style.backdropFilter = "blur(5px)";	
-				}
-			}
-			setTimeout(function() {
-				document.getElementById("menu").style.top = "0"
-				document.getElementById("arrow-container").style.bottom = "30px"
-				window.addEventListener("scroll", isScrolling, false);
-			}, 200)
-		}, 300)
-	}, 50)
+var Secoes = ["equipe", "fotos", "competicoes", "contato"];
+
+for (let i = 0; i < (Secoes.length + 12); i++) {
+	if(i < 4) {
+		document.getElementById(""+ Secoes[i] +"-btn").setAttribute("onclick", "Scroll(Secoes["+i+"])");
+	} else if(i < 8) {
+		document.getElementById(""+ Secoes[i-4] +"-btn-mobile").setAttribute("onclick", "Scroll(Secoes["+(i-4)+"])");
+	} else {
+		document.getElementById("competicao-"+ (i-8) +"").setAttribute("onclick", "Redirect("+(i-8)+")");
+	}
 }
 
-window.addEventListener('beforeunload', function() {
-	localStorage.setItem("autoScroll", 0);
-	localStorage.setItem("scrollPosition", window.pageYOffset);
-});
+if(localStorage.getItem("scrollPosition") == 0) {
+	document.getElementById("carregando").style.opacity = "1";
+}
 
-function logo() {
+function Carregando() {
+	
+	// if(localStorage.getItem("scrollPosition") != 0) {
+	// 	document.getElementById("carregando").style.display = "none"
+	// 	document.getElementById("conteudo").style.display = "initial"
+	// 	document.body.style.opacity = "0"
+	// 	window.scrollTo(0, localStorage.getItem("scrollPosition"))
+	// 	setTimeout(() => {
+	// 		document.body.style.transition = "opacity 1s"
+	// 		document.body.style.opacity = "1"
+	// 		if (localStorage.getItem("scrollPosition") < 10) {
+	// 			document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
+	// 			document.getElementById("menu").style.backdropFilter = "none";
+	// 		} else {
+	// 			document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+	// 			document.getElementById("menu").style.backdropFilter = "blur(5px)";
+	// 		}
+	// 		localStorage.setItem("scrollPosition", 0);
+	// 	}, 50);
+	// } else {
+	// 	document.body.style.transition = "opacity 0.3s"
+	// 	document.body.style.opacity = "0"
+	// 	setTimeout(function() {
+	// 		document.getElementById("carregando").style.display = "none"
+	// 		document.getElementById("conteudo").style.display = "initial"
+	// 		window.scrollTo(0, localStorage.getItem("scrollPosition"))
+	// 		document.body.style.transition = "opacity 1s"
+	// 		document.body.style.opacity = "1"
+	// 	}, 300)
+	// }
+	// setTimeout(function() {
+	// 	document.getElementById("menu").style.top = "0"
+	// 	window.addEventListener("scroll", function Menu() {requestAnimationFrame(isScrolling);}, false);
+	// 	//window.addEventListener("scroll", function Animar() {requestAnimationFrame(Visivel);}, false);
+	// }, 500)
+}
+
+function Logo() {
 	if (window.scrollY < 10) {
-		window.open("file:///C:/Users/lucas/Desktop/gewinner/index.html", "_self");
+		window.open("https://lucasfischer.com.br/gewinner/index.html", "_self");
 	} else {
-		scroll("inicial")
+		Scroll("inicial")
 	}
 }
 
-window.addEventListener("scroll", isElementInViewport)
-
-var estado = [false, false, false]
-
-function isElementInViewport () {
-	if (!estado[0] && document.getElementById("jorge").getBoundingClientRect().top < (window.innerHeight/1.5)) {
-		document.getElementById("jorge").style.marginLeft = "0"
-		document.getElementById("jorge").style.opacity = "1"
-		estado[0] = true;
-	}
-	if (!estado[1] && document.getElementById("fischer").getBoundingClientRect().top < (window.innerHeight/1.5)) {
-		document.getElementById("fischer").style.opacity = "1"
-		estado[1] = true;
-	}
-	if ( !estado[2] &&document.getElementById("theodoro").getBoundingClientRect().top < (window.innerHeight/1.5)) {
-		document.getElementById("theodoro").style.marginLeft = "0"
-		document.getElementById("theodoro").style.opacity = "1"
-		estado[2] = true;
-	}
-	if (estado[0] && estado.every((val, i, arr) => val === arr[0] )) {
-		window.removeEventListener("scroll", isElementInViewport)
-	}
+function isElementVisible(el) {
+	var rect = document.getElementById(el).getBoundingClientRect();
+	return rect.bottom > 0 &&
+	rect.right > 0 &&
+	rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+	rect.top < (window.innerHeight || document.documentElement.clientHeight);
 }
 
-function fechaAviso() {
-	document.getElementById("aviso").style.opacity = "0";
-	document.getElementById("aviso").style.transform = "scale(0.5)";
-	setTimeout(() => {
-		document.getElementById("aviso").style.display = "none";
-		carregar();
-	}, 500);
+// function Visivel() {
+// 	if(document.getElementById("jorge").getBoundingClientRect().top < (window.innerHeight/1.5)) {
+// 		document.getElementById("jorge").style.marginLeft = "0";
+// 		document.getElementById("jorge").style.opacity = "1";
+// 	}
+// 	if(document.getElementById("fischer").getBoundingClientRect().top < (window.innerHeight/1.5)) {
+// 		document.getElementById("fischer").style.opacity = "1";
+// 	}
+// 	if(document.getElementById("theodoro").getBoundingClientRect().top < (window.innerHeight/1.5)) {
+// 		document.getElementById("theodoro").style.marginLeft = "0";
+// 		document.getElementById("theodoro").style.opacity = "1";
+// 	}
+// }
+
+var Redirect_Links = ["regional2018", "nacional2018", "regional2019", "nacional2019", "regional2020", "nacional2020", "regional2021", "nacional2021"];
+
+function Redirect(Pagina) {
+	localStorage.setItem("scrollPosition", window.pageYOffset);
+	console.log(Pagina);
+	window.open("https://lucasfischer.com.br/gewinner/paginas/"+Redirect_Links[Pagina]+"", "_self");
 }
--- gewinner/paginas/javascript/scriptPag.js
+++ gewinner/paginas/javascript/scriptPag.js
@@ -1,62 +1,25 @@
-var paraCima = true;
-var bloquear = false;
-
-function scroll(id) {
-	document.getElementById(id).scrollIntoView({ block: 'start',  behavior: 'smooth' });
-}
-
-function seta() {
-	var ids = ["parte1", "parte2"]
-	bloquear = true;
-	for (var i = 0; i < ids.length; i++) {
-		if (paraCima && document.getElementById(ids[i]).getBoundingClientRect().top > 200) {
-			scroll(ids[i])
-			break;
-		} else if (!paraCima && document.getElementById(ids[i]).getBoundingClientRect().top > -200) {
-			scroll(ids[i-1])
-			break;
-		}
-	}
-}
-
 var lastScrollTop = window.pageYOffset;
 
 window.addEventListener("scroll", function(){
-	if (!bloquear) {
-		var st = window.pageYOffset || document.documentElement.scrollTop;
-		if (st > lastScrollTop) {
-			document.getElementById("botaoVoltar").style.opacity = "0"
-			document.getElementById("arrow").style.cursor = "initial";
-			document.getElementById("arrow-container").style.bottom = "-40px";
-		} else {
-			document.getElementById("botaoVoltar").style.opacity = "1"
-			document.getElementById("arrow").style.cursor = "pointer";
-			document.getElementById("arrow-container").style.bottom = "30px";
-		}
-		lastScrollTop = st <= 0 ? 0 : st;		
-	}
-}, false);
-
-var timer = null;
-window.addEventListener('scroll', function() {
-	if (timer !== null) {
-		clearTimeout(timer);
-	}
-	timer = setTimeout(function() {
-		if (bloquear) {
-			bloquear = false;
-		}
-	}, 70);
-}, false);
-
-window.addEventListener('beforeunload', function() {
-	localStorage.setItem("autoScroll", 1);
-})
-
-function carregou() {
-	setTimeout(function() {
-		document.getElementById("arrow-container").style.bottom = "30px"
-		document.getElementById("botaoVoltar").style.transition = "opacity 0.5s"
+	var st = window.pageYOffset || document.documentElement.scrollTop;
+	if (st > lastScrollTop+0.5) {
+		document.getElementById("botaoVoltar").style.opacity = "0"
+	} else {
 		document.getElementById("botaoVoltar").style.opacity = "1"
-	}, 200)
+	}
+	lastScrollTop = st <= 0 ? 0 : st;
+}, false);
+
+function Carregou() {
+	document.body.style.transition = "opacity 0.3s"
+	document.body.style.opacity = "0"
+	setTimeout(function() {
+		document.getElementById("conteudo").style.display = "initial"
+		document.body.style.transition = "opacity 1s"
+		document.body.style.opacity = "1"
+		setTimeout(function() {
+			document.getElementById("botaoVoltar").style.transition = "opacity 0.5s"
+			document.getElementById("botaoVoltar").style.opacity = "1"
+		}, 200)
+	}, 300)
 }
