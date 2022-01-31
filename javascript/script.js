205f983f772d52660144e57d2dc35d3362c7f766
LucasFischer-max <lucasfischerw@gmail.com>
2022-01-31 11:52:44 -0300

    Novo Site!
    - Mudança total do Design, mantendo o mesmo layout do Site antigo
    - Atualização de imagens e textos
    - Melhorias a serem feitas: suavização do menu, desaparecimento da seta ao terminar scroll
    - Simplificação do código, que foi todo refeito

--- javascript/script.js
+++ javascript/script.js
@@ -1,257 +1,54 @@
-//Carregamento
+var Sidebar_Open = false;
 
-async function preLoad(url) {
-	var img = new Image();
-	img.src = url;
-	img.onload = function() {return;}
+function OpenMenu() {
+    if(!Sidebar_Open) {
+        document.getElementById("sidebar").style.right = "0";
+        document.getElementById("menu").style.animation = "menu-animation 1s ease 2s backwards";
+        document.getElementById("content").style.filter = "blur(5px)";
+        document.getElementById("content").style.opacity = "0.5";
+        Sidebar_Open = true;
+    } else {
+        document.getElementById("sidebar").style.right = "-250px";
+        document.getElementById("menu").style.animation = "menu-animation 1s ease 2s forwards";
+        document.getElementById("content").style.filter = "none";
+        document.getElementById("content").style.opacity = "1";
+        Sidebar_Open = false;
+    }
 }
 
-async function Carregou() {
-	await preLoad("/imagens/fundoInicial.jpg")
-	await preLoad("/imagens/fundoSobre.jpg")
-	await preLoad("/imagens/fundoProgramacao.png")
-	await preLoad("/imagens/fundoProjetos.jpg")
-	await preLoad("/imagens/fundoContato.jpg")
-	await preLoad("/imagens/lucasfischer.jpg")
-	await preLoad("/imagens/programacao.jpg")
-	await preLoad("/imagens/sobre.jpg")
-	await preLoad("/imagens/apostador.jpg")
-	await preLoad("/imagens/emBreve.png")
-	await preLoad("/imagens/genius.jpg")
-	await preLoad("/imagens/gewinner.jpg")
-	await preLoad("/imagens/memoria.jpg")
-	await preLoad("/imagens/tarefas.jpg")
-	await preLoad("/imagens/floresta.jpg")
-	await preLoad("/imagens/seta.png")
-	await preLoad("/imagens/youtube.png")
-	await preLoad("/imagens/telefone.png")
-	await preLoad("/imagens/gmail.png")
-	await preLoad("/imagens/facebook.png")
-	await preLoad("/imagens/instagram.png")
-	await preLoad("/imagens/linkedin.png")
-	document.getElementById("carregando").classList.add("fadeout")
-	document.getElementById("primeiraPag").scrollIntoView({ block: 'start'});
-	setTimeout(function() {
-		document.getElementById('carregando').style.display = "none";
-		document.getElementById('content').style.display = "inherit";
-	}, 300)
-	document.getElementById("content").classList.add("fadein")
-	document.getElementById("menu").classList.add("fadedown");
-	setTimeout(function(){
-		document.getElementById('nomeCima').classList.add("fadeup");
-		document.getElementById('nomeCima').style.opacity = "1.0";
-		document.getElementById("imagemPessoal").classList.add("grow");
-		document.getElementById('imagemPessoal').style.opacity = "1.0";
-		setTimeout(function() {
-			document.getElementById('nome').classList.add("fadein");
-		}, 800)
-	}, 500)
-	setTimeout(function(){
-		document.getElementById("menu").classList.remove("fadedown");
-		document.getElementById("content").classList.remove("fadein");
-	}, 900)
+window.addEventListener("scroll", () => {
+    if(window.scrollY < 20) {
+        document.getElementById("menu").style.height = "100px";
+        document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
+        document.getElementById("menu").style.backdropFilter = "none";  
+    } else {
+        document.getElementById("menu").style.height = "50px";
+        document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, .2)";
+        document.getElementById("menu").style.backdropFilter = "blur(5px)";
+    }
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        document.getElementById("arrow").style.bottom = "-60px";
+    }
+    if(Sidebar_Open) {
+        OpenMenu();
+    }
+});
+
+location.href = "#home";
+
+const Locations = ["about", "knowledge", "projects-title", "projects", "contact"];
+
+function Scroll(Link_Number) {
+    OpenMenu();
+    setTimeout(() => {
+        location.href = "#"+Locations[Link_Number]+"";
+    }, 500);
 }
-
-//Menus
-
-var aberto = false;
-var ultimaCor = "none"
-
-function openNav() {
-	document.getElementById("menu").classList.add("fadeupmenu")
-	setTimeout(function(){
-		if (document.body.clientWidth > 626) {
-			document.getElementById("conteudo").style.marginLeft = "40vw";
-		} else {
-			document.getElementById("conteudo").style.marginLeft = "250px";
-		}
-		document.getElementById("sidebar").style.width = "40vw";
-		document.getElementById("sidebar").style.minWidth = "250px";
-		jQuery('.secao').css('opacity', '0.5');
-		document.body.style.overflowY = "hidden";
-		window.addEventListener("scroll", closeNav);
-		window.addEventListener("touchmove", closeNav);
-		aberto = true;
-	}, 50)
+function Scroll_Arrow() {
+    for(var i = 0; i < Locations.length; i++) {
+        if(document.getElementById(Locations[i]).getBoundingClientRect().top > 200) {
+            document.getElementById(Locations[i]).scrollIntoView({ block: 'start',  behavior: 'smooth' });
+            break;
+        }
+    }
 }
-
-function closeNav() {
-	if (aberto == true) {
-		document.getElementById("menu").classList.remove("fadeupmenu")
-		document.getElementById("botaoExpandir").style.backgroundColor = "transparent !important"
-		document.getElementById("botaoExpandir").style.color = "white !important"
-		document.getElementById("sidebar").style.minWidth = "0px";
-		document.getElementById("sidebar").style.width = "0px";
-		document.getElementById("conteudo").style.marginLeft = "0px";
-		jQuery('.secao').css('opacity', '1');
-		document.getElementById("menu").classList.add("fadedown");
-		window.removeEventListener("scroll", closeNav);
-		window.removeEventListener("touchmove", closeNav);
-		setTimeout(function() {
-			document.getElementById("menu").classList.remove("fadedown");
-		}, 400)
-		document.body.style.overflowY = "inherit";
-		aberto = false;
-	}
-}
-
-var menuEmBaixo = true;
-var lastScrollTop = window.pageYOffset;
-window.addEventListener("scroll", scrolling);
-
-function scrolling() {
-	var st = window.pageYOffset || document.documentElement.scrollTop;
-	if (window.scrollY == 0) {
-		document.getElementById("menu").style.top = "0px";
-		document.getElementById("menu").style.backgroundColor = "rgba(30, 30, 30, 0)";
-		ultimaCor = "rgba(30, 30, 30, 0)"
-		menuEmBaixo = true;
-	} else if (st > lastScrollTop) {
-		document.getElementById("menu").style.top = "-70px";
-		menuEmBaixo = false;
-		setTimeout(function() {
-			document.getElementById("menu").style.backgroundColor = ultimaCor;
-		}, 200)
-	} else {
-		document.getElementById("menu").style.top = "0px";
-		document.getElementById("menu").style.backgroundColor = "rgba(30, 30, 30, 0.5)";
-		ultimaCor = "rgba(30, 30, 30, 0.5)"
-		menuEmBaixo = true;
-	}
-	lastScrollTop = st <= 0 ? 0 : st;
-}
-
-
-//Botões Menu
-
-function scroll(id) {
-	document.getElementById(id).scrollIntoView({ block: 'start',  behavior: 'smooth' });
-}
-
-function Sobre() {
-	document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-	ultimaCor = "rgba(0, 0, 0, 0)"
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("sobre")
-		}, 300)
-	} else {
-		scroll("sobre")
-	}
-}
-
-function Programacao() {
-	document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-	ultimaCor = "rgba(0, 0, 0, 0)"
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("programacao")
-		}, 300)
-	} else {
-		scroll("programacao")
-	}
-}
-
-function Projetos() {
-	document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-	ultimaCor = "rgba(0, 0, 0, 0)"
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("projetos")
-		}, 300)
-	} else {
-		scroll("projetos")
-	}
-}
-
-function Contato() {
-	document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-	ultimaCor = "rgba(0, 0, 0, 0)"
-	if (aberto == true) {
-		closeNav()
-		setTimeout(function() {
-			scroll("contato")
-		}, 300)
-	} else {
-		scroll("contato")
-	}
-}
-
-function Nome() {
-	if (window.scrollY == 0) {
-		window.open("https://lucasfischer.com.br/index.html", "_self");
-	} else {
-		document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0)";
-		ultimaCor = "rgba(0, 0, 0, 0)"
-		scroll("primeiraPag")
-	}
-}
-
-function projeto(id) {
-	if (id == 1) {
-		window.open("https://lucasfischer.com.br/gewinner");
-	} else if (id == 2) {
-		window.open("https://lucasfischer.com.br/projetos/genius/index.html")
-	} else if (id == 3) {
-		window.open("https://lucasfischer.com.br/projetos/memoria/index.html")
-	} else if (id == 4) {
-		window.open("https://lucasfischer.com.br/projetos/apostador/index.html")
-	} else if (id == 5) {
-		window.open("https://lucasfischer.com.br/projetos/tarefas/index.html")
-	}
-}
-
-var scrollPosition;
-var posicaoMenu;
-
-function todosOsProjetos(funcao) {
-	if (funcao == "abrir") {
-		scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-		if (menuEmBaixo == true) {
-			posicaoMenu = "baixo"
-		} else {
-			posicaoMenu = "cima"
-		}
-		document.getElementById('content').classList.remove("fadeinfast");
-		document.getElementById('todosOsProjetos').classList.remove("fadeout");
-		document.getElementById('content').classList.add("fadeout");
-		setTimeout(function() {
-			document.getElementById('content').style.display = "none";
-			document.getElementById('todosOsProjetos').classList.add("fadeinfast");
-			document.getElementById('todosOsProjetos').style.display = "inherit";
-			document.getElementById("todosOsProjetos").scrollIntoView({ block: 'start'});
-		}, 300)
-	} else {
-		document.getElementById('todosOsProjetos').classList.remove("fadeinfast");
-		document.getElementById('content').classList.remove("fadeout");
-		document.getElementById('todosOsProjetos').classList.add("fadeout");
-		setTimeout(function() {
-			document.getElementById('todosOsProjetos').style.display = "none";
-			document.getElementById('content').classList.add("fadeinfast");
-			document.getElementById('content').style.display = "inherit";
-			if (posicaoMenu == "baixo") {
-				setTimeout(function() {
-					document.getElementById("menu").style.top = "0px";
-					document.getElementById("menu").style.backgroundColor = "rgba(30, 30, 30, 0.5)";
-					ultimaCor = "rgba(30, 30, 30, 0.5)"
-					menuEmBaixo = true;
-				}, 20)	
-			} else {
-				document.getElementById("menu").style.top = "-70px";
-				menuEmBaixo = false;
-			}
-			window.scrollTo(0, scrollPosition);
-		}, 300)
-	}
-}
-
-//window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-	//if (event.matches) {
-		//Escuro
-	//} else {
-		//Claro
-	//}
-//})
