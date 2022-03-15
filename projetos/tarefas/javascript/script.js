fb77d141b90e6b0c73264b34aa60d85b5f34c370
LucasFischer-max <lucasfischerw@gmail.com>
2022-03-14 22:36:32 -0300

    Update - Responsivo e Animações

--- projetos/tarefas/javascript/script.js
+++ projetos/tarefas/javascript/script.js
@@ -28,6 +28,6 @@ function Provas() {
 	Reset();
 	document.getElementById("aulas").style.display = "none";
 	document.getElementById("tarefas").style.display = "none";
-	document.getElementById("provas").style.display = "block";
+	document.getElementById("provas").style.display = "flex";
 	document.getElementById("provas-button").style.backgroundColor = "white";
 }
