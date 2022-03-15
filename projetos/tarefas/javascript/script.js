0fc898261742d25cc4a22a3dc69bdfc91f5796d8
LucasFischer-max <lucasfischerw@gmail.com>
2022-03-14 21:33:49 -0300

    Update

--- projetos/tarefas/javascript/script.js
+++ projetos/tarefas/javascript/script.js
@@ -1,18 +1,33 @@
+function Reset() {
+	document.getElementById("tarefas-button").style.backgroundColor = "transparent";
+	document.getElementById("aulas-button").style.backgroundColor = "transparent";
+	document.getElementById("provas-button").style.backgroundColor = "transparent";
+	document.body.style.overflow = "hidden"
+	setTimeout(() => {
+		document.body.style.overflow = "auto"	
+	}, 350);
+}
 
 function Tarefas() {
+	Reset();
 	document.getElementById("aulas").style.display = "none";
 	document.getElementById("provas").style.display = "none";
 	document.getElementById("tarefas").style.display = "flex";
+	document.getElementById("tarefas-button").style.backgroundColor = "white";
 }
 
 function Aulas() {
+	Reset();
 	document.getElementById("provas").style.display = "none";
 	document.getElementById("tarefas").style.display = "none";
-	document.getElementById("aulas").style.display = "block";
+	document.getElementById("aulas").style.display = "flex";
+	document.getElementById("aulas-button").style.backgroundColor = "white";
 }
 
 function Provas() {
+	Reset();
 	document.getElementById("aulas").style.display = "none";
 	document.getElementById("tarefas").style.display = "none";
 	document.getElementById("provas").style.display = "block";
+	document.getElementById("provas-button").style.backgroundColor = "white";
 }
