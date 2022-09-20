e362c18e2f9588fd3a747edc024b1183b2f74d9e
LucasFischer-max <lucasfischerw@gmail.com>
2022-09-20 19:13:06 -0300

    Update

--- projetos/Form/javascript/main.js
+++ projetos/Form/javascript/main.js
@@ -40,42 +40,63 @@ var data_js = {
     "access_token": "5nwxvbw5lnt539t1130619c1"
 };
 function js_onSuccess() {
-    window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+    window.location = "sucess-page.html?message=Email+Successfully+Sent%21&isError=0";
 }
 function js_onError(error) {
     window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
 }
 var sendButton = document.getElementById("js_send");
 function js_send() {
-    sendButton.value='Sending…';
-    sendButton.disabled=true;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState == 4 && request.status == 200) {
-            js_onSuccess();
-        } else
-        if(request.readyState == 4) {
-            js_onError(request.response);
-        }
-    };
-    var subject = "Client Request from: " + document.querySelector("#" + form_id_js + " [name='subject']").value;
-    data_js['subject'] = subject;
-    var stringResult = "";
-    for (let index = 0; index < document.getElementsByTagName("a").length; index++) {
-        if(document.getElementsByTagName("a")[index].classList.contains("selected")) {
-            if(stringResult.length == 0) {
-                stringResult += document.getElementsByTagName("a")[index].textContent;
-            } else {
-                stringResult += ", " + document.getElementsByTagName("a")[index].textContent;
+    if(document.querySelector("#" + form_id_js + " [name='subject']").value != "" && document.querySelector("#" + form_id_js + " [name='subject']").value.includes("@")) {
+        sendButton.value='Sending…';
+        sendButton.disabled=true;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                js_onSuccess();
+            } else
+            if(request.readyState == 4) {
+                js_onError(request.response);
+            }
+        };
+        var subject = "Client Request from: " + document.querySelector("#" + form_id_js + " [name='subject']").value;
+        data_js['subject'] = subject;
+        var stringResult = "";
+        for (let index = 0; index < document.getElementsByTagName("a").length; index++) {
+            if(document.getElementsByTagName("a")[index].classList.contains("selected")) {
+                if(stringResult.length == 0) {
+                    stringResult += document.getElementsByTagName("a")[index].textContent;
+                } else {
+                    stringResult += ", " + document.getElementsByTagName("a")[index].textContent;
+                }
             }
         }
+        data_js['text'] = stringResult;
+        var params = toParams(data_js);
+        request.open("POST", "https://postmail.invotes.com/send", true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send(params);
+        return false;
+    } else {
+        document.getElementById("advice-email").classList.add("fail");
+        document.getElementById("message-log").innerHTML = "Please insert a valid Email Adress";
+        document.getElementById("advice-email").style.display = "block";
+        setTimeout(() => {
+            document.getElementsByName("subject")[0].style.borderColor = "red";
+            document.getElementsByName("subject")[0].style.backgroundColor = "rgb(251 191 191)";
+            document.getElementById("advice-email").style.opacity = "1";
+            document.getElementById("advice-email").style.bottom = "35px";
+            setTimeout(() => {
+                document.getElementsByName("subject")[0].style.borderColor = "black";
+                document.getElementsByName("subject")[0].style.backgroundColor = "white";
+                document.getElementById("advice-email").style.opacity = "0";
+                document.getElementById("advice-email").style.bottom = "0";
+                setTimeout(() => {
+                    document.getElementById("advice-email").style.display = "block";
+                }, 500);
+            }, 1500);
+        }, 350);
     }
-    data_js['text'] = stringResult;
-    var params = toParams(data_js);
-    request.open("POST", "https://postmail.invotes.com/send", true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.send(params);
-    return false;
 }
 sendButton.onclick = js_send;
 function toParams(data_js) {
