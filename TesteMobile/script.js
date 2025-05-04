8dc8d6406dc8ee7880a687c36772367457318ad4
Lucas Fischer Wachholz <lucasfischerw@gmail.com>
2025-05-04 00:23:18 -0300

    Update

--- TesteMobile/script.js
+++ TesteMobile/script.js
@@ -2,11 +2,28 @@ const video = document.getElementById("video-input");
 const canvas = document.getElementById("canvas-output");
 
 async function initialize() {
-    console.log(navigator);
+    const socket = new WebSocket("ws://192.168.4.1/ws");
+
+    socket.onopen = () => {
+    console.log("WebSocket connected");
+    };
+
+    socket.onerror = (e) => {
+    console.error("WebSocket error", e);
+    };
+
+    function sendFastData(errorPorcent) {
+        if (socket.readyState === WebSocket.OPEN) {
+            socket.send(errorPorcent.toFixed(2));
+        }
+    }
+
+
     const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
+        video: {
+            facingMode: { exact: "environment" } // força a câmera traseira
+        },
         audio: false,
-        facingMode: 'environment',
     });
 
     if (!stream) {
@@ -27,17 +44,41 @@ async function initialize() {
     const centerX = src.cols / 2;
     const centerY = src.rows / 2;
 
+    let height = 100;
+    let y = src.rows - height; // começa 100 pixels antes de terminar a imagem
+    let x = 0;
+    let width = src.cols;
+
+    var ultimoCentro = centerX;
+
     function processVideo() {
         let begin = Date.now();
 
         cap.read(src);
-        cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+        let rect = new cv.Rect(x, y, width, height);
+        let cropped = src.roi(rect);
+        cv.cvtColor(cropped, gray, cv.COLOR_RGBA2GRAY);
         cv.threshold(gray, thresh, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU);
 
         cv.erode(thresh, thresh, kernel, new cv.Point(-1, -1), 5);
         cv.dilate(thresh, thresh, kernel, new cv.Point(-1, -1), 9);
 
         cv.findContours(thresh,contours,hierarchy,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE);
+
+        // Conversão para HSV
+        let imgHSV = new cv.Mat();
+        cv.cvtColor(cropped, imgHSV, cv.COLOR_BGR2HSV);
+        // Definição dos limites HSV
+        let lower = cv.matFromArray(1, 1, cv.CV_8UC3, [60, 125, 65]);
+        let upper = cv.matFromArray(1, 1, cv.CV_8UC3, [90, 255, 255]);
+        // Aplicação da máscara
+        let masked_image = new cv.Mat();
+        cv.inRange(imgHSV, lower, upper, masked_image);
+
+        // Liberação de memória
+        lower.delete();
+        upper.delete();
+        // NÃO delete `frame` nem `masked_image` antes do imshow
 
         var bestContourIndex = 0;
         var smallestCenterDistance = 1000000;
@@ -50,7 +91,7 @@ async function initialize() {
             const cx = M.m10 / M.m00;
             const cy = M.m01 / M.m00;
 
-            const distance = cx - centerX;
+            const distance = cx - ultimoCentro;
             
             if (Math.abs(distance) < Math.abs(smallestCenterDistance)) {
                 smallestCenterDistance = distance;
@@ -62,7 +103,7 @@ async function initialize() {
         // Draw the best contour
         if (bestContour) {
             const color = new cv.Scalar(0, 255, 0, 255);
-            cv.drawContours(src, contours, bestContourIndex, color, 2, cv.LINE_8, hierarchy, 100);
+            cv.drawContours(cropped, contours, bestContourIndex, color, 2, cv.LINE_8, hierarchy, 100);
         
             // Draw the center
             const M = cv.moments(bestContour);
@@ -71,7 +112,8 @@ async function initialize() {
             const center = new cv.Point(cx, cy);
             const radius = 5;
             const colorCenter = new cv.Scalar(0, 0, 255, 255); // Red color
-            cv.circle(src, center, radius, colorCenter, -1);
+            cv.circle(cropped, center, radius, colorCenter, -1);
+            ultimoCentro = cx;
         
             // Compute the min area rect and get the angle
             const rotatedRect = cv.minAreaRect(bestContour); // returns {center, size, angle}
@@ -91,14 +133,15 @@ async function initialize() {
             document.getElementById("error").innerText = `Error: ${errorPorcent.toFixed(2)}%`;
             document.getElementById("angle").innerText = `Angle: ${angle.toFixed(2)}`;
             document.getElementById("width").innerText = `Width: ${rotatedRect.size.width.toFixed(2)}`;
+            document.getElementById("teste").innerText = `Width: ${ultimoCentro}`;
+
+
+            sendFastData(errorPorcent);
         }
 
-        if(contours.size() === 0) {
-            document.getElementById("text").innerText = "No contours found";
-        }
 
         // Show the result
-        cv.imshow("canvas-output", src);
+        cv.imshow("canvas-output", masked_image);
 
         // Schedule the next frame
         let delay = 1000 / FPS - (Date.now() - begin);
